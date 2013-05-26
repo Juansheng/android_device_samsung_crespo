c473ea4d4c510f5a2924e66eece978c877bdfb61
Jay Wu <cn.fyodor@gmail.com>
2013-05-26 22:51:37 +0800

    Do not check JNI for the kernel

--- device_base.mk
+++ device_base.mk
@@ -63,7 +63,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.com.google.clientidbase=android-google \
 	ro.com.android.wifi-watchlist=GoogleGuest \
 	ro.setupwizard.enterprise_mode=1 \
-	ro.com.android.dataroaming=false
+	ro.com.android.dataroaming=false \
+	ro.kernel.android.checkjni=0
 
 # Prebuilt kl and kcm keymaps
 PRODUCT_COPY_FILES += \
